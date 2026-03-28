de0248641e1814f3589a253e2d339d1ebeb6d295
Chronoss0518 <sinyusuke5000@gmail.com>
2026-03-28 20:19:23 +0900

    選択可能フラグを用意して,一部のTextを選択可能に変更

--- lib/Component/text_block.dart
+++ lib/Component/text_block.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 class TextBlock extends StatelessWidget
 {
 
-  const TextBlock(this.drawText,{this.fontSize,this.textColor,this.backGroundColor,this.padding,this.margin,super.key});
+  const TextBlock(this.drawText,{this.fontSize,this.textColor,this.backGroundColor,this.padding,this.margin,this.isSelect = false,super.key});
 
   final String drawText;
   final double? fontSize;
@@ -15,9 +15,15 @@ class TextBlock extends StatelessWidget
   final EdgeInsetsGeometry? padding;
   final EdgeInsetsGeometry? margin;
 
+  final bool isSelect;
+
   @override
   Widget build(BuildContext context)
   {
-    return Container(padding: padding,margin: margin,color: backGroundColor,child:Text(drawText,style: TextStyle(color: textColor,fontSize: fontSize)));
+
+    return Container(padding: padding,margin: margin,color: backGroundColor,child:
+    !isSelect ?
+    Text(drawText,style: TextStyle(color: textColor,fontSize: fontSize)) :
+    SelectableText(drawText,style: TextStyle(color: textColor,fontSize: fontSize)));
   }
 }
